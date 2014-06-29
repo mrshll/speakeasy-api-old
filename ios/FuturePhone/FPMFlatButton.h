c31dd7e09fa60d2817d33a4b599da72c36394bbd
mmoutenot <mmoutenot@gmail.com>
2014-06-29 09:19:28 -0400

    add animator and flat button files

---
+++ ios/FuturePhone/FPMFlatButton.h
@@ -0,0 +1,12 @@
+//
+//  Created by Marshall Moutenot on 6/14/14.
+//  Copyright (c) 2014 futurephone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FPMFlatButton : UIButton
+
++ (instancetype)button;
+
+@end
---
+++ ios/FuturePhone/FPMPresentingAnimator.h
@@ -0,0 +1,10 @@
+//
+//  Created by Marshall Moutenot on 6/14/14.
+//  Copyright (c) 2014 futurephone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FPMPresentingAnimator : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
